d547350221586e04251c9c111288f0d47950e05a
temurkhvedelidze <temurkhvedelidze1@gmail.com>
2025-07-02 16:47:32 +0400

    მნიშვნელოვანი ცვლილებები!

---
+++ puzzle.py
@@ -0,0 +1,22 @@
+class Puzzle:
+    def __init__(self, puzzle_id, fen, moves, rating):
+        self.puzzle_id = puzzle_id
+        self.fen = fen
+        self.moves = moves
+        self.rating = rating
+
+    def fen_to_board(self):
+        board = []
+        setup_str = self.fen.split(" ")[0]
+        for each in setup_str:
+            if each.isdigit():
+                for i in range(int(each)):
+                    board.append("")
+
+            elif each != "/":
+                board.append(each)
+
+        return board
+
+    def __str__(self):
+        return f"ID = {self.puzzle_id}\nFEN = {self.fen}\nMOVES = {self.moves}\nRATING = {self.rating}"
---
+++ puzzles_database.py
@@ -0,0 +1,28 @@
+import sqlite3
+from puzzle import Puzzle
+from random import *
+
+class PuzzlesDatabase:
+    def __init__(self, database_name="data.sqlite"):
+        self.conn = sqlite3.connect(database_name)
+        self.cursor = self.conn.cursor()
+        print(self.get_random_puzzle())
+
+    def get_all_puzzles(self):
+        self.cursor.execute("SELECT * FROM lichess_puzzle_transformed WHERE Themes LIKE ?", ('%mateIn%',))
+        return self.cursor.fetchall()
+
+    def get_random_puzzle(self):
+        puzzles = self.get_all_puzzles()
+        random_puzzle = puzzles[randint(0, len(puzzles)-1)]
+
+        puzzle_id = random_puzzle[0]
+        fen = random_puzzle[2]
+        moves = random_puzzle[3]
+        rating = random_puzzle[4]
+
+        puzzle = Puzzle(puzzle_id, fen, moves, rating)
+        return puzzle
+
+
+db1 = PuzzlesDatabase()
